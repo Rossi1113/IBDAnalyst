89a5a8e3f77d6e731d298399028c52adc0608d0c
XI LUO <luoxi1113@gmail.com>
2018-09-29 10:51:58 -0700

    Add stock and stocklist model

---
+++ src/main/java/org/luoxi/ibd/model/input/StockList.java
@@ -0,0 +1,40 @@
+package org.luoxi.ibd.model.input;
+
+import java.util.List;
+
+import org.luoxi.ibd.model.InputSpreadsheet;
+
+import com.google.common.collect.Lists;
+
+/**
+ *
+ * Data pojo to represent a raw spreadsheet from IBD, e.g IBD 50.xls
+ *
+ * @author luoxi
+ *
+ */
+public class StockList implements InputSpreadsheet {
+	private String name;  // Spreadsheet name
+	private List<Stock> stocks;  // List of stocks in this spreadsheet
+
+	public StockList() {
+		stocks = Lists.newArrayList();
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Stock> getStocks() {
+		return stocks;
+	}
+	public void setStocks(List<Stock> stocks) {
+		this.stocks = stocks;
+	}
+
+	public void addStock(Stock stock) {
+		stocks.add(stock);
+	}
+}
