b3ad5b0b5c180e2e4fe66bf9a74b5d0cbbb62dc1
XI LUO <luoxi1113@gmail.com>
2018-09-30 11:24:27 -0700

    Add second level stock list model

---
+++ src/main/java/org/luoxi/ibd/model/input/IBD50AndSectorLeaderStockList.java
@@ -0,0 +1,41 @@
+package org.luoxi.ibd.model.input;
+
+import java.util.List;
+
+import org.luoxi.ibd.model.InputSpreadsheet;
+import org.luoxi.ibd.model.output.StockAnalyzeResult;
+
+import com.google.common.collect.Lists;
+
+/**
+ *
+ * Data pojo to represent a result spreadsheet, e.g MM_dd_yy_ibd50_plus_sector_leader.xls
+ *
+ * @author luoxi
+ *
+ */
+public class IBD50AndSectorLeaderStockList implements InputSpreadsheet {
+	private String name;   // The result spreadsheet name
+	private List<StockAnalyzeResult> stocks;  // The stock listed in this result spreadsheet
+
+	public IBD50AndSectorLeaderStockList() {
+		stocks = Lists.newArrayList();
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<StockAnalyzeResult> getStocks() {
+		return stocks;
+	}
+	public void setStocks(List<StockAnalyzeResult> stocks) {
+		this.stocks = stocks;
+	}
+
+	public void addStock(StockAnalyzeResult s) {
+		stocks.add(s);
+	}
+}
