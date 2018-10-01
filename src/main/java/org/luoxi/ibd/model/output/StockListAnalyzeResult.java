be19106c875e7f88266eb687bf37274c85e09c87
XI LUO <luoxi1113@gmail.com>
2018-10-01 08:06:41 -0700

    Add StockList output pojo

---
+++ src/main/java/org/luoxi/ibd/model/output/StockListAnalyzeResult.java
@@ -0,0 +1,59 @@
+package org.luoxi.ibd.model.output;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.luoxi.ibd.model.OutputSpreadsheet;
+import org.luoxi.ibd.model.input.Stock;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+/**
+ * A data pojo to represent a bunch of stock analyze result.
+ * The major purpose for this pojo is to generate the result spreadsheet.
+ *
+ * @author luoxi
+ *
+ */
+public class StockListAnalyzeResult implements OutputSpreadsheet {
+
+	// Key is the symbol of the stock
+	private Map<String, StockAnalyzeResult> result;
+
+	public StockListAnalyzeResult() {
+		super();
+		result = Maps.newHashMap();
+	}
+
+	public Map<String, StockAnalyzeResult> getResult() {
+		return result;
+	}
+
+	/**
+	 * Add a new or existing stock analyze result
+	 * @param stock
+	 */
+	public void addStockAnalyzeResult(Stock stock, String involvedSpreadsheet) {
+		String symbol = stock.getSymbol();
+		// If exists, update occurrence and involvedSpreadsheets
+		if (result.containsKey(symbol)) {
+			if (!result.get(symbol).getInvolvedSpreadsheets().contains(involvedSpreadsheet)) {
+				result.get(symbol).setOccurrence(result.get(symbol).getOccurrence() + 1);
+				result.get(symbol).getInvolvedSpreadsheets().add(involvedSpreadsheet);
+			}
+		} else {
+			StockAnalyzeResult stockAnalyzeResult = new StockAnalyzeResult(
+					symbol, stock.getCompanyName(), 1, Sets.newHashSet(involvedSpreadsheet));
+			result.put(symbol, stockAnalyzeResult);
+		}
+	}
+
+	/**
+	 * Add an existing stock analyze result entry.
+	 * @param existingStockAnalyzeResult
+	 */
+	public void addExistingStockAnalyzeResult(Entry<String, StockAnalyzeResult> existingStockAnalyzeResult) {
+		result.put(existingStockAnalyzeResult.getKey(), existingStockAnalyzeResult.getValue());
+	}
+}
