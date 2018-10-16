3e18c72964242b5c53c027a89430da59d4cdd234
XI LUO <luoxi1113@gmail.com>
2018-10-16 08:08:48 -0700

    Quote enricher

---
+++ src/main/java/org/luoxi/ibd/enrich/QuotePerformanceEnricher.java
@@ -0,0 +1,109 @@
+package org.luoxi.ibd.enrich;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.log4j.Logger;
+import org.luoxi.ibd.config.ConfigFactory;
+import org.luoxi.ibd.exception.QuoteException;
+import org.luoxi.ibd.model.OutputSpreadsheet;
+import org.luoxi.ibd.model.TimePeriod;
+import org.luoxi.ibd.model.output.IBD50AndSectorLeaderStockAnalyzeResult;
+import org.luoxi.ibd.model.output.IBD50AndSectorLeaderStockListAnalyzeResult;
+import org.luoxi.ibd.model.output.StockAnalyzeResult;
+import org.luoxi.ibd.model.output.StockListAnalyzeResult;
+import org.luoxi.ibd.services.QuoteService;
+
+/**
+ * Add quote performance for each stock.
+ * The way we calculate this is :
+ * (Current Price - Old Price) / Old Price
+ * Old price is based on the time period between current date.
+ *
+ * @author luoxi
+ *
+ */
+public class QuotePerformanceEnricher implements Enricher {
+
+	private QuoteService quoteService;
+	private final static Logger logger = Logger.getLogger(QuotePerformanceEnricher.class);
+
+	public QuotePerformanceEnricher() {
+		quoteService = (QuoteService)ConfigFactory.get().getBean("quoteService");
+	}
+
+	/**
+	 * Set performance quote for each stock
+	 */
+	public OutputSpreadsheet enrich(OutputSpreadsheet outputSpreadsheet) {
+		// Update the context first
+		EnricherUtil.updateContext(this.getClass().getSimpleName());
+
+		// TODO  A better way to handle this
+		if (outputSpreadsheet instanceof StockListAnalyzeResult) {
+			StockListAnalyzeResult result = (StockListAnalyzeResult)outputSpreadsheet;
+
+			Iterator<Entry<String, StockAnalyzeResult>> iterator = result.getResult().entrySet().iterator();
+			while (iterator.hasNext()) {
+				StockAnalyzeResult singleStockResult = iterator.next().getValue();
+				double oneWeekPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.ONE_WEEK);
+				double oneMonthPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.ONE_MONTH);
+				double threeMonthsPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.THREE_MONTHS);
+				double sixMonthsPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.SIX_MONTHS);
+
+				singleStockResult.getQuotePerformance().put(TimePeriod.ONE_WEEK, oneWeekPerformance);
+				singleStockResult.getQuotePerformance().put(TimePeriod.ONE_MONTH, oneMonthPerformance);
+				singleStockResult.getQuotePerformance().put(TimePeriod.THREE_MONTHS, threeMonthsPerformance);
+				singleStockResult.getQuotePerformance().put(TimePeriod.SIX_MONTHS, sixMonthsPerformance);
+			}
+
+			return result;
+		} else if (outputSpreadsheet instanceof IBD50AndSectorLeaderStockListAnalyzeResult) {
+			IBD50AndSectorLeaderStockListAnalyzeResult result = (IBD50AndSectorLeaderStockListAnalyzeResult)outputSpreadsheet;
+
+			Iterator<Entry<String, IBD50AndSectorLeaderStockAnalyzeResult>> iterator = result.getResult().entrySet().iterator();
+			while(iterator.hasNext()) {
+				IBD50AndSectorLeaderStockAnalyzeResult singleStockResult = iterator.next().getValue();
+				double oneWeekPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.ONE_WEEK);
+				double oneMonthPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.ONE_MONTH);
+				double threeMonthsPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.THREE_MONTHS);
+				double sixMonthsPerformance = getQuotePerformance(singleStockResult.getSymbol(), TimePeriod.SIX_MONTHS);
+
+				singleStockResult.setOneWeekPerformance(oneWeekPerformance);
+				singleStockResult.setOneMonthPerformance(oneMonthPerformance);
+				singleStockResult.setThreeMonthsPerformance(threeMonthsPerformance);
+				singleStockResult.setSixMonthsPerformance(sixMonthsPerformance);
+			}
+
+			return result;
+		}
+
+		return null;
+	}
+
+	private double getQuotePerformance(String symbol, TimePeriod timePeriod) {
+
+		Pair<Double, Double> quotes;
+		try {
+			quotes = quoteService.getHistoryQuotes(symbol, timePeriod);
+		} catch (QuoteException e) {
+			logger.error("Fail to get quote performance for " + symbol + " for " + timePeriod.getName(), e);
+			return Double.NaN;
+		}
+		log(symbol, quotes, timePeriod);
+
+		return (quotes.getLeft() - quotes.getRight()) / quotes.getRight();
+	}
+
+	private void log(String symbol, Pair<Double, Double> quotes, TimePeriod timePeriod) {
+		logger.info("Get " + symbol + " for " + timePeriod.getNumberOfWeeks() + " weeks");
+		logger.info("Current price is " + quotes.getLeft());
+		logger.info("Old price is " + quotes.getRight());
+	}
+
+	public static void main(String[] args) {
+		QuotePerformanceEnricher q = new QuotePerformanceEnricher();
+		System.out.println(q.getQuotePerformance("CRM", TimePeriod.ONE_MONTH));
+	}
+}
