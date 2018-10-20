2849987cc0583fcb8d2f0a76bd5b6d2838382ff5
XI LUO <luoxi1113@gmail.com>
2018-10-20 10:23:06 -0700

    Context Implementation

---
+++ src/main/java/org/luoxi/ibd/context/ContextImpl.java
@@ -0,0 +1,60 @@
+package org.luoxi.ibd.context;
+
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
+public class ContextImpl implements Context {
+
+	private String scannerName;
+	private String analyzerName;
+	private Set<String> filterNames;
+	private Set<String> enricherNames;
+	private String printerName;
+
+	public ContextImpl() {
+		filterNames = Sets.newHashSet();
+		enricherNames = Sets.newHashSet();
+	}
+
+	public String getScannerName() {
+		return scannerName;
+	}
+
+	public String getAnalyzerName() {
+		return analyzerName;
+	}
+
+	public Set<String> getFilterNames() {
+		return filterNames;
+	}
+
+	public Set<String> getEnricherNames() {
+		return enricherNames;
+	}
+
+	public String getPrinterName() {
+		return printerName;
+	}
+
+	public void setScannerName(String scannerName) {
+		this.scannerName = scannerName;
+	}
+
+	public void setAnalyzerName(String analyzerName) {
+		this.analyzerName = analyzerName;
+	}
+
+	public void addFilterName(String filterName) {
+		this.filterNames.add(filterName);
+	}
+
+	public void addEnricherName(String enricherName) {
+		this.enricherNames.add(enricherName);
+	}
+
+	public void setPrinterName(String printerName) {
+		this.printerName = printerName;
+	}
+
+}
